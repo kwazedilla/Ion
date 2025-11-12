96398cbd2ae5aabc4f33c74c6ee53a4f345571fa
kwazedilla <kwazedilla@gmail.com>
2025-11-11 16:51:56 -0800

    more wip

--- server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/type/weapon/blaster/NewBlaster.kt
+++ server/src/main/kotlin/net/horizonsend/ion/server/features/custom/items/type/weapon/blaster/NewBlaster.kt
@@ -3,7 +3,6 @@ package net.horizonsend.ion.server.features.custom.items.type.weapon.blaster
 import io.papermc.paper.datacomponent.DataComponentTypes
 import io.papermc.paper.datacomponent.item.ItemAttributeModifiers
 import net.horizonsend.ion.server.configuration.NewBlasterBalancing
-import net.horizonsend.ion.server.configuration.PVPBalancingConfiguration.EnergyWeapons.Balancing
 import net.horizonsend.ion.server.core.registration.IonRegistryKey
 import net.horizonsend.ion.server.features.custom.items.CustomItem
 import net.horizonsend.ion.server.features.custom.items.component.CustomComponentTypes
@@ -15,7 +14,7 @@ import net.horizonsend.ion.server.features.custom.items.util.ItemFactory
 import net.horizonsend.ion.server.miscellaneous.utils.updateData
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
-import net.minecraft.world.entity.LivingEntity
+import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import java.util.function.Supplier
@@ -34,11 +33,11 @@ abstract class NewBlaster<T : NewBlasterBalancing>(
         addComponent(CustomComponentTypes.MOD_MANAGER, ModManager(modLimit))
 
         addComponent(CustomComponentTypes.LISTENER_PLAYER_INTERACT, rightClickListener(this@NewBlaster) { event, _, item ->
-            fire(event.player as LivingEntity, item)
+            fire(event.player, item)
         })
 
         addComponent(CustomComponentTypes.LISTENER_PLAYER_SWAP_HANDS, playerSwapHandsListener(this@NewBlaster) { event, _, item ->
-            reload(event.player as LivingEntity, item)
+            reload(event.player, item)
         })
     }
 
@@ -46,10 +45,34 @@ abstract class NewBlaster<T : NewBlasterBalancing>(
         if (shooter is Player) {
 
         }
+
+        fireProjectiles(shooter)
     }
 
     open fun fireProjectiles(livingEntity: LivingEntity) {
+        val location = livingEntity.eyeLocation.clone()
 
+        location.y -= 0.125
+
+        if (balancing.spreadDegrees > 0) {
+            val radians = balancing.spreadDegrees * Math.PI / 180
+            location.direction.rotateAroundX(radians).rotateAroundY(radians).rotateAroundZ(radians)
+        }
+
+        /*
+        RayTracedParticleProjectile(
+            location,
+            livingEntity,
+            balancing,
+            DUST,
+            balancing.explosiveShot,
+            DustOptions(
+                getParticleColor(livingEntity),
+                balancing.particleSize
+            ),
+            balancing.soundWhizz,
+        ).fire()
+         */
     }
 
     open fun reload(livingEntity: LivingEntity, blasterItem: ItemStack) {
